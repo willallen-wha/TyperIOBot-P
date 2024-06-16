cb8e8d8a3938ecfd74a17e0708b4501e1453458d
Will Allen <willallen.wha@gmail.com>
2024-06-15 23:55:46 -0600

    Confirmed functionality of keyboard library

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # TyperIOBot-P
 Python implementation of Typer.io TAS
+
+## DEPENDENCIES
+Keyboard: https://github.com/boppreh/keyboard
---
+++ prototyping/keyboardTest.py
@@ -0,0 +1,18 @@
+import keyboard as kb
+import time
+
+# Test of the keyboard module - wait for space key to be pressed, then wait
+# three more seconds for the round to start, and type the provided snippet.
+# At the minute, the snippet is typed into prompt.txt ahead of time.
+
+# Store the snippet as a string
+sourceFile = open("prototyping/prompt.txt", "rt")
+text = sourceFile.read()
+
+# Wait for the space key
+kb.wait("space")
+# Wait three seconds for the round to start, and an extra 1.5 for the "press
+# space to start" prompt to go away
+time.sleep(4.5)
+# Then type the snippet
+kb.write(text)
